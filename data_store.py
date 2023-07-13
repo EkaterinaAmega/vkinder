744d89c28b8a97c3bc7f5f59fd896028c81943d3
EkaterinaAmega <139164554+EkaterinaAmega@users.noreply.github.com>
2023-07-13 12:13:35 +0300

    Правки по диплому

--- data_store.py
+++ data_store.py
@@ -9,15 +9,18 @@ from config import db_url_object
 metadata = MetaData()
 Base = declarative_base()
 
+
 class Viewed(Base):
-    __tablename__ = 'viewed'
+    __tablename__ = "viewed"
     profile_id = sq.Column(sq.Integer, primary_key=True)
     worksheet_id = sq.Column(sq.Integer, primary_key=True)
+
 
 engine = create_engine(db_url_object)
 Base.metadata.create_all(engine)
 
 # Добавление записей из БД
+
 
 def database_add_user(profile_id, worksheet_id):
     with Session(engine) as session:
@@ -25,10 +28,17 @@ def database_add_user(profile_id, worksheet_id):
         session.add(to_bd)
         session.commit()
 
+
 # Извлечение записей из БД
+
 
 def database_check_user(profile_id, worksheet_id):
     with Session(engine) as session:
-        from_bd = session.query(Viewed).filter(
-            Viewed.profile_id == profile_id, Viewed.worksheet_id == worksheet_id).first()
+        from_bd = (
+            session.query(Viewed)
+            .filter(
+                Viewed.profile_id == profile_id, Viewed.worksheet_id == worksheet_id
+            )
+            .first()
+        )
         return True if from_bd else False
